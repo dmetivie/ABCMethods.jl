3aea64601925a7cd0a4059378f687ff9aed2058d
David Métivier <46794064+dmetivie@users.noreply.github.com>
2025-01-07 17:39:44 +0100

    comment MSE check

--- src/NN.jl
+++ src/NN.jl
@@ -1,11 +1,11 @@
 # Training 
 
 """
-	train_NN([rng], model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), adtype)
+	train_NN([rng], model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), val_loss = compute_loss, adtype)
 	train_NN(train_state, epochs, dataset, dataset_val, compute_loss; dev = cpu_device(), adtype)
-Train the `model` or `train_state` and comute at each epoch the training and testing loss.
+Train the `model` or `train_state` and comute at each epoch the training with `compute_loss` and validation loss with `val_loss`.
 """
-function train_NN(train_state::TrainState, epochs, dataset, dataset_val, compute_loss; dev = cpu_device(), adtype)
+function train_NN(train_state::TrainState, epochs, dataset, dataset_val, compute_loss; dev = cpu_device(), val_loss = compute_loss, adtype)
     ps = train_state.parameters
     st = train_state.states
     model = train_state.model
@@ -13,10 +13,13 @@ function train_NN(train_state::TrainState, epochs, dataset, dataset_val, compute
     ## Validation Loss
     losses_train = Float32[]
     x_val, y_val = dataset_val |> dev
-    losses_val = Float32[first(compute_loss(model, ps, st, (x_val, y_val)))]
+    losses_val = Float32[first(val_loss(model, ps, st, (x_val, y_val)))]
+    # mse_loss_val = MSELoss()(first(first(model(x_val, ps, st))), y_val)
+
     loss = rand(Float32) # just to define loss in outer loop scope # probably better ways to do that
     best_test_state = train_state
-
+    # @info "Epoch 0                     validation_loss = $(round(losses_val[1], digits = 4)) mse_val_loss = $(round(mse_loss_val, digits = 4)) Best model so far"
+    @info "Epoch 0                     validation_loss = $(round(losses_val[1], digits = 4)) Best model so far"
     ## Training loop
     for epoch in 1:epochs
         issave = false
@@ -26,28 +29,29 @@ function train_NN(train_state::TrainState, epochs, dataset, dataset_val, compute
         end
         ps = train_state.parameters
         st = train_state.states
-        loss_val = first(compute_loss(model, ps, st, (x_val, y_val)))
+        loss_val = first(val_loss(model, ps, st, (x_val, y_val)))
+        # mse_loss_val = MSELoss()(first(first(model(x_val, ps, st))), y_val)
         if loss_val < minimum(losses_val)
             best_test_state = train_state
             issave = true
         end
         append!(losses_train, loss)
         append!(losses_val, loss_val)
+        # @info "Epoch $epoch train_loss = $(round(loss, digits = 4)) validation_loss = $(round(loss_val, digits = 4)) mse_val_loss = $(round(mse_loss_val, digits = 4)) $(issave ? "Best model so far" : " ")"
         @info "Epoch $epoch train_loss = $(round(loss, digits = 4)) validation_loss = $(round(loss_val, digits = 4)) $(issave ? "Best model so far" : " ")"
     end
     return best_test_state, losses_train, losses_val
 end
 
-function train_NN(rng::AbstractRNG, model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), adtype)
+function train_NN(rng::AbstractRNG, model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), val_loss = compute_loss, adtype)
     ## Set up models
-    
     ps, st = Lux.setup(rng, model) |> dev    
 
     train_state = TrainState(model, ps, st, opt)
-    return train_NN(train_state, epochs, dataset, dataset_val, compute_loss; dev = dev, adtype = adtype)
+    return train_NN(train_state, epochs, dataset, dataset_val, compute_loss; dev = dev, adtype = adtype, val_loss = val_loss)
 end
 
-train_NN(model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), adtype) = train_NN(Random.Xoshiro(0), model, epochs, dataset, dataset_val, compute_loss; opt=opt, dev = dev, adtype = adtype)
+train_NN(model, epochs, dataset, dataset_val, compute_loss; opt=Adam(0.001f0), dev = cpu_device(), val_loss = compute_loss, adtype) = train_NN(Random.Xoshiro(0), model, epochs, dataset, dataset_val, compute_loss; opt=opt, dev = dev, val_loss = val_loss, adtype = adtype)
 
 function train_step(train_state, xy, compute_loss; adtype)
     ## Calculate the gradient of the objective
