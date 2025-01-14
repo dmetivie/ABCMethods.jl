e6ba1e2b4f7d2ef351caa048a46e257a044ddcc4
David Métivier <46794064+dmetivie@users.noreply.github.com>
2025-01-14 16:46:38 +0100

    inSmthList functions

--- src/confidence_regions.jl
+++ src/confidence_regions.jl
@@ -125,8 +125,12 @@ Using the ABC selected sample to estimate the posterior distribution, it compute
 """
 function inABCEllipsoid(ABCresults::AbstractVector, θ_test::AbstractMatrix, thr)
     N = length(ABCresults)
+    return count(inABCEllipsoidList(ABCresults, θ_test, thr)) / N
+end
+
+function inABCEllipsoidList(ABCresults::AbstractVector, θ_test::AbstractMatrix, thr)
+    N = length(ABCresults)
     @assert N == size(θ_test, 2)
-    ν = size(θ_test, 1) # dimension of the parameters
     InOrNot = zeros(Bool, N)
     for (i, x) in enumerate(ABCresults)
         m = mean(x, dims = 2)[:, 1]
@@ -134,7 +138,7 @@ function inABCEllipsoid(ABCresults::AbstractVector, θ_test::AbstractMatrix, thr
         ## squared Mahalanobis distance
         @views InOrNot[i] = dot(m - θ_test[:, i], inv(C), m - θ_test[:, i]) < thr^2
     end
-    return count(InOrNot) / N
+    return InOrNot
 end
 
 function AreaABCEllipsoid(ABCresults::AbstractVector, thr)
@@ -163,10 +167,14 @@ end
 Compute the `α`-Highest Density Region (so far only in 2D) and verify is the test data is in.
 """
 function inHDR(ABCresults::AbstractVector, θ_test, α)
+    return count(inHDRList(ABCresults::AbstractVector, θ_test, α))/length(ABCresults)
+end
+
+function inHDRList(ABCresults::AbstractVector, θ_test, α)
     @assert length(ABCresults) == size(θ_test, 2)
     if size(θ_test, 1) == 2
         iks_thrs = compute_thresholds2D.(ABCresults, α)
-        return count([pdf(iks_thrs[i][1], c[1], c[2]) > iks_thrs[i][2] for (i,c) in enumerate(eachcol(θ_test))])/length(ABCresults)
+        return [pdf(iks_thrs[i][1], c[1], c[2]) > iks_thrs[i][2] for (i,c) in enumerate(eachcol(θ_test))]
     else
         @warn "kde not yet implemented for $(size(result, 1))-Dimension"
     end
@@ -175,86 +183,14 @@ end
 
 #* Rectangle *#
 
-inABCRectangle(df_results) = count(prod(hcat([df.:q_low .≤ df.:θ .≤ df.:q_high for df in df_results]...), dims=2)) / nrow(df_results[1])
+inABCRectangle(df_results) = count(inABCRectangleList(df_results)) / nrow(df_results[1])
+inABCRectangleList(df_results) = prod(hcat([df.:q_low .≤ df.:θ .≤ df.:q_high for df in df_results]...), dims=2)
+
 AreaABCRectangle(df_results) = prod(hcat([df.:q_high - df.:q_low for df in df_results]...), dims=2)
 
-# """
-#     confidence_ellipse2D(x::AbstractVector, y::AbstractVector, s; n_points = 100)
-#     confidence_ellipse2D(X::AbstractMatrix, s; n_points = 100)
-# Create the points forming the `s`-confidence ellipse of `X` (or `x` and `y`).
-# To have a `α`-confidence region choose `s = quantile(Chi(2), α)` 
-# Can be used with `Plots.jl` and other plotting libraries.
-# ```julia
-# using Plots
-# using Distributions
-# Σ = [1 0; 0 1]
-# X = rand(MvNormal(Σ), 2000)
-# α = 0.95
-# s = quantile(Chi(2), α)
-# scatter(X[1,:], X[2,:])
-# plot!(confidence_ellipse2D(X', s))
-# ```
-# Idea from https://github.com/CarstenSchelp/CarstenSchelp.github.io/blob/master/LICENSE
-# """
-# function confidence_ellipse2D(x::AbstractVector, y::AbstractVector, s; n_points = 100, cov_matrix = cov(hcat(x, y)))
-#     if length(x) != length(y)
-#         throw(ArgumentError("x and y must be the same size"))
-#     end
-
-    
-#     pearson = cov_matrix[1, 2] / sqrt(cov_matrix[1, 1] * cov_matrix[2, 2])
-
-#     ell_radius_x = sqrt(1 + pearson)
-#     ell_radius_y = sqrt(1 - pearson)
-
-#     scale_x = sqrt(cov_matrix[1, 1]) * s
-#     mean_x = mean(x)
-
-#     scale_y = sqrt(cov_matrix[2, 2]) * s
-#     mean_y = mean(y)
-
-#     # Generate the ellipse points
-#     t = LinRange(0, 2π, n_points)
-#     ellipse_x = ell_radius_x * cos.(t)
-#     ellipse_y = ell_radius_y * sin.(t)
-
-#     # Rotation and scaling transformation
-#     rotation_matrix = [cos(π/4) -sin(π/4); 
-#                        sin(π/4)  cos(π/4)]
-#     rotated_ellipse = rotation_matrix * hcat(ellipse_x, ellipse_y)'
-#     scaled_ellipse = hcat(scale_x * rotated_ellipse[1, :], scale_y * rotated_ellipse[2, :])
-
-#     # # Translate the ellipse
-#     translated_ellipse_x = scaled_ellipse[:, 1] .+ mean_x
-#     translated_ellipse_y = scaled_ellipse[:, 2] .+ mean_y
-
-#     return (translated_ellipse_x, translated_ellipse_y)
-# end
-
-# confidence_ellipse2D(X::AbstractMatrix, s; n_points = 100, cov_matrix = cov(X)) = confidence_ellipse2D(X[:,1], X[:,2], s; n_points = n_points, cov_matrix = cov_matrix)
-
-# function areaellipse(X, s)
-#     cov_matrix = cov(X)
-#     return areaellipseΣ(cov_matrix, s)
-# end
-
-# function areaellipseΣ(cov_matrix, s)
-#     n = size(cov_matrix, 1)
-#     σ = [sqrt(cov_matrix[i, i]) for i in 1:n]
-#     cor_matrix = cov2cor(cov_matrix)
-#     ρ = filter(!iszero, triu(cor_matrix)-Diagonal(cor_matrix))#[cor_matrix[i,j] for j in i+1:n, j in 1:n-1]
-#     return prod(sqrt(1-r^2) for r in ρ)*prod(σ)*s^2
-# end
-
-# function areaellipseΣ(cov_matrix, s)
-#     ρ = cov_matrix[1, 2] / sqrt(cov_matrix[1, 1] * cov_matrix[2, 2])
-#     σ1 = sqrt(cov_matrix[1, 1])
-#     σ2 = sqrt(cov_matrix[2, 2])
-#     return sqrt(1-ρ^2)*σ1*σ2*s^2
-# end
-# aa = [ABCMethods.areaellipse(results_posterior_cnn[i]',quantile(Chi(2), 0.95))
+# aa = [ABCMethods.ellipsoid_area(results_posterior_cnn[i]',quantile(Chi(2), 0.95))
 # for i in eachindex(results_posterior_cnn)]
-# bb = [ABCMethods.areaellipseΣ(MC_testD[2][:, :, i],qhatD) for i in eachindex(results_posterior_cnn)]
+# bb = [ABCMethods.ellipsoid_area(MC_testD[2][:, :, i],qhatD) for i in eachindex(results_posterior_cnn)]
 # a = score_MultiDim(θ_test_Norm, MC_testD...) .< qhatD
 # b = prod(hcat([df.:q_low .≤ df.:θ .≤ df.:q_high for df in df_cnn_result]...), dims=2)
 # sortperm(bb)
